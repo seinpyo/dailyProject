09e90efa1ad09866e9f16e18ece664eb1391f592
seinpyo <91402281+seinpyo@users.noreply.github.com>
2022-07-17 14:33:29 +0900

    first commit

---
+++ dailyProject/src/test/java/com/web/dailyProject/DailyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.dailyProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
